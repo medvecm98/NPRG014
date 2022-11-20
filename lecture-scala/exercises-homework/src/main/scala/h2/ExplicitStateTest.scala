f7516c541263d78ae42a9909f8e598b9c3785dd5
Michal Medvecky <michalmedvecky98@gmail.com>
2022-11-20 16:22:50 +0100

    hw2

--- lecture-scala/exercises-homework/src/main/scala/h2/ExplicitStateTest.scala
+++ lecture-scala/exercises-homework/src/main/scala/h2/ExplicitStateTest.scala
@@ -10,54 +10,83 @@ Person Joe aged 24
 
 */
 
-/*
 package h2
 
 import scala.collection.mutable.ListBuffer
 
-
 trait WithExplicitState:
-  /* add necessary declarations here */
+  type EState
 
-  protected def state: /* add the correct type here */
-  protected def state_=(state: /* add the correct type here */): Unit
+  protected var actualState : EState
+
+  protected def state: EState
+
+  protected def state_=(state: EState): Unit
 
 
 class PersonState(val name: String, val age: Int)
 
 class Person extends WithExplicitState:
-  /* Implement this class. It should have no knowledge of the trait History. It should use instances of PersonState as the state. */
+  type EState = PersonState
 
+  var actualState : PersonState = new PersonState("", -1)
 
-type RGBColor = (Int, Int, Int)
+  override def state: PersonState = actualState
+
+  override def state_=(state: PersonState): Unit =
+    actualState = state
+
+  def setName(value: String) : this.type =
+    state = new PersonState(value, state.age)
+    this
+
+  def setAge(value: Int) : this.type =
+    state = new PersonState(state.name, value)
+    this
+
+  override def toString() : String =
+    s"Person ${state.name} is ${state.age} years old."
+
+type RGBColor = (Int, Int , Int)
 class ThingState(val name: String, val color: RGBColor)
 
 class Thing extends WithExplicitState:
-  /* Implement this class. It should have no knowledge of the trait History. It should use instances of ThingState as the state. */
+  type EState = ThingState
 
+  var actualState : ThingState = new ThingState("", (0, 0, 0))
 
+  override def state: ThingState = actualState
 
-trait History:
-    /* Add necessary declarations here. This trait should have no knowledge of classes Person, Thing, PersonState, ThingState.
-       It should depend only on the trait WithExplicitState.
-    */
+  override def state_=(state: ThingState): Unit = 
+    actualState = state
 
-    val hist = ListBuffer.empty[/* add the correct type here */]
+  def setName(value: String) : this.type =
+    state = new ThingState(value, state.color)
+    this
 
-    def checkpoint(): /* add the correct type here */ =
+  def setColor(value: RGBColor) : this.type = 
+    state = new ThingState(state.name, value)
+    this
+
+  override def toString() : String =
+    s"Thing named \"${state.name}\" has RGB encoded color: ${state.color}."
+
+trait History extends WithExplicitState:
+    val hist = ListBuffer.empty[EState]
+
+    def checkpoint(): this.type =
       hist.append(state)
       this
 
     def history = hist.toList
 
-    def restoreTo(s: /* add the correct type here */): /* add the correct type here */ =
+    def restoreTo(s: EState): this.type =
       state = s
       this
 
 
 object ExplicitStateTest:
   def main(args: Array[String]): Unit =
-    // The inferred type of variable "john" should be "Person & History".
     val john = (new Person with History).setName("John Doe").setAge(24).checkpoint()
 
     println(john)
@@ -70,7 +99,6 @@ object ExplicitStateTest:
     john.restoreTo(johnsPrevState)
     println(john)
 
-    // The inferred type of variable "box" should be "Thing & History".
     val box = new Thing with History
     box.setName("Box")
     box.setColor((255, 0, 0))
@@ -80,7 +108,18 @@ object ExplicitStateTest:
     joe.restoreTo(johnsPrevState).setName("Joe")
     println(joe)
 
-    // The line below must not compile. It should complain about an incompatible type.
-    // box.restoreTo(prevState)
-
-*/
+    //The line below must not compile. It should complain about an incompatible type.
+    //box.restoreTo(johnsPrevState) //errors out, as it should:
+    // [info] compiling 1 Scala source to /home/michal/mff/NPRG014/lecture-scala/exercises-homework/target/scala-3.0.0/classes ...
+    // [error] -- [E007] Type Mismatch Error: /home/michal/mff/NPRG014/lecture-scala/exercises-homework/src/main/scala/h2/ExplicitStateTest.scala:125:18 
+    // [error] 125 |    box.restoreTo(johnsPrevState) //errors out, as it should:
+    // [error]     |                  ^^^^^^^^^^^^^^
+    // [error]     |Found:    (johnsPrevState : john.EState)
+    // [error]     |Required: box.EState²
+    // [error]     |
+    // [error]     |where:    EState  is a type in class Person which is an alias of h2.PersonState
+    // [error]     |          EState² is a type in class Thing which is an alias of h2.ThingState
+    // [error] one error found
+    // [error] one error found
+    // [error] (Compile / compileIncremental) Compilation failed
+    // [error] Total time: 1 s, completed Nov 20, 2022, 4:15:24 PM
